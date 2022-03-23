ae8bc3ddfc46c5a1a2bf202e839650ebc2fcc2e3
Vanessa Fotso <vfotso@mitre.org>
2022-03-23 15:51:41 -0400

    L04_2 solution

--- src/main/java/com/fhirintcourse/u2/L04_2_CreateUSCoreImmunization.java
+++ src/main/java/com/fhirintcourse/u2/L04_2_CreateUSCoreImmunization.java
@@ -1,27 +1,71 @@
 package com.fhirintcourse.u2;
 
+import org.hl7.fhir.r4.model.CodeableConcept;
+import org.hl7.fhir.r4.model.DateTimeType;
+import org.hl7.fhir.r4.model.Immunization;
+import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.Reference;
+import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
+import org.hl7.fhir.r4.model.Immunization.ImmunizationStatus;
+import org.hl7.fhir.r4.model.Narrative.NarrativeStatus;
+import org.hl7.fhir.r5.model.IdType;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.IParser;
+
 public class L04_2_CreateUSCoreImmunization {
 
-    public String CreateUSCoreR4Immunization
-    (  
-    String ServerEndPoint,
-    String IdentifierSystem,
-    String IdentifierValue,
-    String ImmunizationStatusCode,
-    String ImmunizationDateTime,
-    String ProductCVXCode,
-    String ProductCVXDisplay,
-    String ReasonCode)
-    
-    {       
-        String aux="";
-        return aux;  
+  public String CreateUSCoreR4Immunization(
+      String ServerEndPoint,
+      String IdentifierSystem,
+      String IdentifierValue,
+      String ImmunizationStatusCode,
+      String ImmunizationDateTime,
+      String ProductCVXCode,
+      String ProductCVXDisplay,
+      String ReasonCode)
+
+  {
+    String aux = "";
+    BundleEntryComponent ptEntry = new L01_1_FetchDemographics().getPatient(ServerEndPoint, IdentifierSystem,
+        IdentifierValue);
+    Patient pt = ptEntry == null ? null : (Patient) ptEntry.getResource();
+    if (pt != null) {
+      Immunization immunization = new Immunization();
+      // Set profile
+      immunization.getMeta().addProfile("http://hl7.org/fhir/us/core/StructureDefinition/us-core-immunization");
+      // Add narrative
+      immunization.getText().setStatus(NarrativeStatus.GENERATED).setDivAsString("Redacted");
+      // Status code
+      immunization.setStatus(ImmunizationStatus.fromCode(ImmunizationStatusCode));
+      // Status reason
+      if (!ReasonCode.isEmpty()) {
+        CodeableConcept value = new CodeableConcept();
+        value.addCoding().setSystem("http://terminology.hl7.org/CodeSystem/v3-ActReason").setCode(ReasonCode);
+        immunization.setStatusReason(value);
+      }
+      // Vaccine code
+      CodeableConcept vaccineCode = new CodeableConcept();
+      vaccineCode.addCoding().setCode(ProductCVXCode).setDisplay(ProductCVXDisplay)
+          .setSystem("http://hl7.org/fhir/sid/cvx");
+      immunization.setVaccineCode(vaccineCode);
+      // OccurrenceDateTime
+      immunization.setOccurrence(new DateTimeType(ImmunizationDateTime));
+      // Patient
+      Reference patient = new Reference(new IdType(pt.getId()).getId());
+      patient.setDisplay(pt.getNameFirstRep().getNameAsSingleString());
+      immunization.setPatient(patient);
+      // primary source
+      immunization.setPrimarySource(false);
+
+      // Encode in json
+      FhirContext ctx = FhirContext.forR4();
+      IParser parser = ctx.newJsonParser();
+      aux = parser.encodeResourceToString(immunization);
+    } else {
+      aux = "Error:Patient_Not_Found";
     }
-         
-
-
-
-
-
+    return aux;
+  }
 
 }
--- src/test/java/com/fhirintcourse/u2/L04_2_Tests.java
+++ src/test/java/com/fhirintcourse/u2/L04_2_Tests.java
@@ -13,122 +13,115 @@ import ca.uhn.fhir.parser.IParser;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.client.api.ServerValidationModeEnum;
 
-
 @DisplayName("Tests for U2-L04_2:Create US Core Immunization")
 
 class L04_2_Tests {
-    
-    String ObtainResult(String IdentifierValue,
-    String ImmunizationStatusCode, 
-    String ImmunizationDateTime,
-    String ProductCVXCode, 
-    String ProductCVXDisplay,
-    String ReasonCode)
-    
+
+  String ObtainResult(String IdentifierValue,
+      String ImmunizationStatusCode,
+      String ImmunizationDateTime,
+      String ProductCVXCode,
+      String ProductCVXDisplay,
+      String ReasonCode)
+
+  {
+    String aux = "";
+    L04_2_CreateUSCoreImmunization L04_2 = new L04_2_CreateUSCoreImmunization();
+    Config c = new Config();
+    String ServerEndPoint = c.ServerEndpoint();
+    String IdentifierSystem = c.PatientIdentifierSystem();
+
+    try {
+      aux = L04_2.CreateUSCoreR4Immunization(ServerEndPoint, IdentifierSystem, IdentifierValue, ImmunizationStatusCode,
+          ImmunizationDateTime, ProductCVXCode, ProductCVXDisplay, ReasonCode);
+
+    } catch (Exception e) {
+      aux = e.getMessage();
+    }
+
+    return aux;
+  }
+
+  @Test
+  @DisplayName("L04_2_T01:Patient Not Found")
+  void L04_2_T01() {
+
+    String IdentifierValue = "L04_2_T01";
+    String ExpectedResult = "Error:Patient_Not_Found";
+    String ImmunizationStatusCode = "";
+    String ImmunizationDateTime = "";
+    String ProductCVXCode = "";
+    String ProductCVXDisplay = "";
+    String ReasonCode = "";
+    String result = ObtainResult(IdentifierValue,
+        ImmunizationStatusCode, ImmunizationDateTime, ProductCVXCode, ProductCVXDisplay, ReasonCode);
+    assertEquals(ExpectedResult.toUpperCase(), result.toUpperCase());
+  }
+
+  @Test
+  @DisplayName("L04_2_T02:Immunization Completed")
+  void L04_2_T02() {
+
+    String IdentifierValue = "L04_2_T02";
+    String ImmunizationStatusCode = "completed";
+    String ImmunizationDateTime = "2021-10-25";
+    String ProductCVXCode = "173";
+    String ProductCVXDisplay = "cholera, BivWC";
+    String ReasonCode = "";
+    String result = ObtainResult(IdentifierValue,
+        ImmunizationStatusCode, ImmunizationDateTime, ProductCVXCode, ProductCVXDisplay, ReasonCode);
+
+    Config c = new Config();
+    String val = ValidateResource(c.ServerEndpoint(), result);
+    assertEquals(val, "OK");
+
+  }
+
+  @Test
+  @DisplayName("L04_2_T03:Immunization Not Done")
+  void L04_2_T03() {
+
+    String IdentifierValue = "L04_2_T02";
+    String ImmunizationStatusCode = "not-done";
+    String ImmunizationDateTime = "2021-10-30T10:30:00Z";
+    String ProductCVXCode = "158";
+    String ProductCVXDisplay = "COVID-19, mRNA, LNP-S, PF, 100 mcg/0.5 mL dose";
+    String ReasonCode = "IMMUNE";
+
+    String result = ObtainResult(IdentifierValue,
+        ImmunizationStatusCode, ImmunizationDateTime, ProductCVXCode, ProductCVXDisplay, ReasonCode);
+
+    Config c = new Config();
+    String val = ValidateResource(c.ServerEndpoint(), result);
+    assertEquals(val, "OK");
+  }
+
+  private String ValidateResource(String ServerEndPoint, String resourceTextJson) {
+    FhirContext ctx = FhirContext.forR4();
+    ctx.getRestfulClientFactory().setServerValidationMode(ServerValidationModeEnum.NEVER);
+    ctx.setPerformanceOptions(PerformanceOptionsEnum.DEFERRED_MODEL_SCANNING);
+    IGenericClient client = ctx.newRestfulGenericClient(ServerEndPoint);
+    IParser parser = ctx.newJsonParser();
+    Immunization ob = parser.parseResource(Immunization.class, resourceTextJson);
+    Parameters inParams = new Parameters();
     {
-        String aux="";
-        L04_2_CreateUSCoreImmunization L04_2 = new L04_2_CreateUSCoreImmunization();
-        Config c=new Config();
-        String ServerEndPoint = c.ServerEndpoint();
-        String IdentifierSystem = c.PatientIdentifierSystem();
-       
-        try {
-            aux = L04_2.CreateUSCoreR4Immunization(ServerEndPoint, IdentifierSystem, IdentifierValue
-            , ImmunizationStatusCode, ImmunizationDateTime, ProductCVXCode, ProductCVXDisplay, ReasonCode)
-            ;
-            
-
-        } 
-        catch (Exception e) {
-            aux=e.getMessage();
-        }
-        
-        return aux;
-    }
-    @Test
-    @DisplayName("L04_2_T01:Patient Not Found")
-    void L04_2_T01() {
-
-        String IdentifierValue = "L04_2_T01";
-        String ExpectedResult = "Error:Patient_Not_Found";
-        String ImmunizationStatusCode="";
-        String ImmunizationDateTime="";
-        String ProductCVXCode="";
-        String ProductCVXDisplay="";
-        String ReasonCode="";
-        String result=ObtainResult(IdentifierValue,
-        ImmunizationStatusCode, ImmunizationDateTime, ProductCVXCode, ProductCVXDisplay, ReasonCode)
-        ;
-        assertEquals(ExpectedResult.toUpperCase(),result.toUpperCase());
-    }
-    @Test
-    @DisplayName("L04_2_T02:Immunization Completed")
-    void L04_2_T02() {
-
-        String IdentifierValue = "L04_2_T02";
-        String ImmunizationStatusCode="completed";
-        String ImmunizationDateTime="2021-10-25";
-        String ProductCVXCode="173";
-        String ProductCVXDisplay="cholera, BivWC";
-        String ReasonCode="";
-        String result=ObtainResult(IdentifierValue,
-        ImmunizationStatusCode, ImmunizationDateTime, ProductCVXCode, ProductCVXDisplay, ReasonCode)
-        ;
-      
-        Config c=new Config();
-        String val=ValidateResource(c.ServerEndpoint(), result);
-        assertEquals(val,"OK");
-        
-       
-    }
-    @Test
-    @DisplayName("L04_2_T03:Immunization Not Done")
-    void L04_2_T03() {
-        
-        String IdentifierValue = "L04_2_T02";
-        String ImmunizationStatusCode="not-done";
-        String ImmunizationDateTime="2021-10-30T10:30:00Z";
-        String ProductCVXCode="207";
-        String ProductCVXDisplay="COVID-19, mRNA, LNP-S, PF, 100 mcg/0.5 mL dose";
-        String ReasonCode="IMMUNE";
-    
-        String result=ObtainResult(IdentifierValue,
-        ImmunizationStatusCode, ImmunizationDateTime, ProductCVXCode, ProductCVXDisplay, ReasonCode)
-        ;
-      
-        Config c=new Config();
-        String val=ValidateResource(c.ServerEndpoint(), result);
-        assertEquals(val,"OK");
+      inParams.addParameter().setName("resource").setResource(ob);
     }
 
-    private String ValidateResource(String ServerEndPoint,String resourceTextJson)
-    {
-        FhirContext ctx = FhirContext.forR4();
-        ctx.getRestfulClientFactory().setServerValidationMode(ServerValidationModeEnum.NEVER);
-        ctx.setPerformanceOptions(PerformanceOptionsEnum.DEFERRED_MODEL_SCANNING);
-        IGenericClient client = ctx.newRestfulGenericClient(ServerEndPoint);
-        IParser parser = ctx.newJsonParser();  
-        Immunization ob=parser.parseResource(Immunization.class, resourceTextJson); 
-        Parameters inParams = new Parameters();  
-        {  
-            inParams.addParameter().setName("resource").setResource(ob);  
-        }  
-    
-        Parameters outParams = client  
-            .operation()  
-            .onType(Immunization.class)  
-            .named("$validate")  
-            .withParameters(inParams)  
-            .execute();  
-    
-     OperationOutcome oo = (OperationOutcome) outParams.getParameter().get(0).getResource();  
-     String auxE="OK";
-     if (!(oo.getIssueFirstRep().getDetails().getText().equals("Validation successful, no issues found")))
-            {
-                auxE="Error:"+oo.getIssueFirstRep().getDetails().getText();
-            }
-            
-     return auxE;
+    Parameters outParams = client
+        .operation()
+        .onType(Immunization.class)
+        .named("$validate")
+        .withParameters(inParams)
+        .execute();
+
+    OperationOutcome oo = (OperationOutcome) outParams.getParameter().get(0).getResource();
+    String auxE = "OK";
+    if (!(oo.getIssueFirstRep().getDetails().getText().equals("Validation successful, no issues found"))) {
+      auxE = "Error:" + oo.getIssueFirstRep().getDetails().getText();
     }
-   
+
+    return auxE;
+  }
+
 }
